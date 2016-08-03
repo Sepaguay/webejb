ba5d6c6cce96be22142749cf6f43fcd998ba5266
Sepaguay <paguaysandra8@gmail.com>
2016-08-03 09:51:50 -0500

    entrega datos con json

--- EJBWEB/EJBWEB-war/src/java/controlador/UsuarioControlador.java
+++ EJBWEB/EJBWEB-war/src/java/controlador/UsuarioControlador.java
@@ -1,23 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package controlador;
 
 import entidades.Uztuser;
+import flexjson.JSONSerializer;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.security.MessageDigest;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import modelo.UztuserFacade;
 import java.util.*;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
 
-/**
- *
- * @author Sandra
- */
 @ManagedBean
 @SessionScoped
 public class UsuarioControlador implements Serializable{
@@ -32,4 +30,60 @@ public class UsuarioControlador implements Serializable{
     {
         return this.uztuserFacade.findAll();
     }
+    
+    public String toJson(List prueba)
+    {
+        JSONSerializer json= new JSONSerializer();
+        return json.serialize(prueba);
+    }
+    
+     public String encriptar(String texto)  {
+ 
+        String secretKey = "qualityinfosolutions"; //llave para encriptar datos
+        String base64EncryptedString = "";
+ 
+        try {
+ 
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+            byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+ 
+            SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+            Cipher cipher = Cipher.getInstance("DESede");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+ 
+            byte[] plainTextBytes = texto.getBytes("utf-8");
+            byte[] buf = cipher.doFinal(plainTextBytes);
+            byte[] base64Bytes = Base64.encodeBase64(buf);
+            base64EncryptedString = new String(base64Bytes);
+ 
+        } catch (Exception ex) {
+        }
+        return base64EncryptedString;
+}
+         public String Desencriptar(String textoEncriptado)throws Exception {
+ 
+        String secretKey = "qualityinfosolutions"; //llave para desenciptar datos
+        String base64EncryptedString = "";
+ 
+        try {
+            byte[] message = Base64.decodeBase64(textoEncriptado.getBytes("utf-8"));
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+            byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+            SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+ 
+            Cipher decipher = Cipher.getInstance("DESede");
+            decipher.init(Cipher.DECRYPT_MODE, key);
+ 
+            byte[] plainText = decipher.doFinal(message);
+ 
+            base64EncryptedString = new String(plainText, "UTF-8");
+ 
+        } catch (Exception ex) {
+        }
+        return base64EncryptedString;
+}
+ 
+    
 }
