174063d917652f994d03ca5847c2d205035d71b6
elvis <elvis69_dark@hotmail.com>
2016-08-26 11:53:24 -0500

    metodos para los proceso

--- EJBWEB/EJBWEB-ejb/src/java/modelo/UztprocFacade.java
+++ EJBWEB/EJBWEB-ejb/src/java/modelo/UztprocFacade.java
@@ -7,9 +7,13 @@
 package modelo;
 
 import entidades.Uztproc;
+import java.math.BigInteger;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -29,4 +33,141 @@ public class UztprocFacade extends AbstractFacade<Uztproc> {
         super(Uztproc.class);
     }
     
+     //Metodo que devuelve el Proceso segun el ID del proceso 
+    public List<Uztproc> findProcByID(BigInteger uztprocId) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE u.uztprocPK.uztprocId = :uztprocId");
+            query.setParameter("uztprocId", uztprocId);
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }
+        
+
+    }
+    ////////////////////////////.
+    
+    //Metodo que devuelve el Proceso segun el ID del Sistema 
+    public List<Uztproc> findProcByIdSis(BigInteger uztsistId) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE u.uztprocPK.uztsistId = :uztsistId");
+            query.setParameter("uztsistId", uztsistId);
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+     //Metodo que devuelve el Proceso segun el ID del Sistema y el ID del proceso
+    public List<Uztproc> findProcByIdSisAndIdProc(BigInteger uztsistId,BigInteger uztprocId) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE u.uztprocPK.uztsistId = :uztsistId "
+                    + "AND u.uztprocPK.uztprocId = :uztprocId");
+            query.setParameter("uztsistId", uztsistId);
+            query.setParameter("uztprocId", uztprocId);
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+    //Metodo que devuelve el Proceso segun el nombre
+    public List<Uztproc> findProcByName(String uztprocNombre) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE UPPER(u.uztprocNombre) LIKE :uztprocNombre");
+            query.setParameter("uztprocNombre", "%"+uztprocNombre.toUpperCase()+"%");
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+     //Metodo que devuelve el Proceso segun el Ejecucion
+    public List<Uztproc> findProcByExecution(String uztprocEjecucion) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE UPPER(u.uztprocEjecucion) LIKE :uztprocEjecucion");
+            query.setParameter("uztprocEjecucion", "%"+uztprocEjecucion.toUpperCase()+"%");
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+     //Metodo que devuelve el Proceso segun Inicio
+    public List<Uztproc> findProcByStart(String uztprocInicio) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE UPPER(u.uztprocInicio) LIKE :uztprocInicio");
+            query.setParameter("uztprocInicio", "%"+uztprocInicio.toUpperCase()+"%");
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+      //Metodo que devuelve el Proceso segun Salida
+    public List<Uztproc> findProcByExit(String uztprocSalida) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE UPPER(u.uztprocSalida) LIKE :uztprocSalida");
+            query.setParameter("uztprocSalida", "%"+uztprocSalida.toUpperCase()+"%");
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+          //Metodo que devuelve el Proceso segun el estado
+    public List<Uztproc> findProcByEstado(Character uztprocEstado) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE u.uztprocEstado = :uztprocEstado");
+            query.setParameter("uztprocEstado", uztprocEstado);
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
+    
+      //Metodo que devuelve el Proceso segun la Descripcion
+    public List<Uztproc> findProcByDescripcion(String uztprocDescripcion) {
+
+        try{
+            Query query = em.createQuery("SELECT u FROM Uztproc u WHERE UPPER(u.uztprocDescripcion) LIKE :uztprocDescripcion");
+            query.setParameter("uztprocDescripcion", "%"+uztprocDescripcion.toUpperCase()+"%");
+            return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }    
+
+    }
+    ////////////////////////////.
 }
