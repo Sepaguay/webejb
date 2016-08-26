a857686baed7fe6e20b2e12745e0f8bdc9c6cf05
elvis <elvis69_dark@hotmail.com>
2016-08-26 09:58:34 -0500

    metodos para el usuario

--- EJBWEB/EJBWEB-ejb/src/java/modelo/UztuserFacade.java
+++ EJBWEB/EJBWEB-ejb/src/java/modelo/UztuserFacade.java
@@ -65,7 +65,8 @@ public class UztuserFacade extends AbstractFacade<Uztuser> {
         try{
             Query query = em.createQuery("Select u from Uztuser u Where u.uztuserId = :value");
             query.setParameter("value", Id);
-            return (Uztuser) query.getSingleResult();
+            List<Uztuser> lista= query.getResultList();
+            return lista.get(0);
         }catch(NoResultException e)
         {
             return null;
@@ -104,7 +105,52 @@ public class UztuserFacade extends AbstractFacade<Uztuser> {
 
     }
     ////////////////////////////.
+    
+     //Metodo que devuelve el usuario segun el Cargo
+    public List<Uztuser> findUserByCargo(String cargo) {
 
+        try{
+        Query query = em.createQuery("SELECT u FROM Uztuser u WHERE UPPER(u.uztuserCargo) LIKE UPPER(:uztuserCargo)");
+        query.setParameter("uztuserCargo", "%"+cargo+"%");
+        return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }
+
+    }
+    ////////////////////////////.
+
+         //Metodo que devuelve el usuario segun el Cargo
+    public List<Uztuser> findUserByEstado(Character estado) {
+
+        try{
+        Query query = em.createQuery("SELECT u FROM Uztuser u WHERE u.uztuserEstado = :uztuserEstado");
+        query.setParameter("uztuserEstado", estado);
+        return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }
+
+    }
+    ////////////////////////////.
+    
+     //Metodo que devuelve el usuario segun el Campus
+    public List<Uztuser> findUserByCampus(String campus) {
+
+        try{
+        Query query = em.createQuery("SELECT u FROM Uztuser u WHERE UPPER(u.uztuserCampus) LIKE UPPER(:uztuserCampus)");
+        query.setParameter("uztuserCampus", "%"+campus+"%");
+        return query.getResultList();
+        }catch(NoResultException e)
+        {
+            return null;
+        }
+
+    }
+    ////////////////////////////.
+    
     // Transforma una List a Json en formato String
     public String toJson(List prueba) {
         JSONSerializer json = new JSONSerializer();
