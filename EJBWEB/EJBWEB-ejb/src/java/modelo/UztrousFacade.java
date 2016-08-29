a136d2620fc21a714cf30d934f1c988b576303f9
Sepaguay <paguaysandra8@gmail.com>
2016-08-29 09:42:26 -0500

    Rous facade

--- EJBWEB/EJBWEB-ejb/src/java/modelo/UztrousFacade.java
+++ EJBWEB/EJBWEB-ejb/src/java/modelo/UztrousFacade.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package modelo;
 
 import entidades.Uztrous;
+import java.math.BigDecimal;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -17,6 +16,7 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class UztrousFacade extends AbstractFacade<Uztrous> {
+
     @PersistenceContext(unitName = "EJBWEB-ejbPU")
     private EntityManager em;
 
@@ -28,5 +28,72 @@ public class UztrousFacade extends AbstractFacade<Uztrous> {
     public UztrousFacade() {
         super(Uztrous.class);
     }
-    
+
+    //MÉTODO QUE DEVUELVE TODOS LOS REGISTROS DE ROLES Y USUARIOS
+    public List<Uztrous> findAllRolUsu() {
+        try {
+            return em.createNamedQuery("Uztrous.findAll").getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    ///MÉTODO PARA ROUS POR ID_ROL
+    public List<Uztrous> findRolById(BigDecimal uztrolId) {
+        try {
+            return em.createNamedQuery("Uztrous.findByUztrolId").setParameter("uztrolId", uztrolId).getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    ///MÉTODO PARA ROUS POR PIDM  USUARIO
+    public List<Uztrous> findRolByPidm(BigDecimal uztuserPidm) {
+        try {
+            return em.createNamedQuery("Uztrous.findByUztuserPidm").setParameter("uztuserPidm", uztuserPidm).getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    ///MÉTODO PARA ROUS POR ESTADO
+    public List<Uztrous> findRousByEstado(Character uztrousEstado) {
+        try {
+            Query query = em.createQuery("SELECT u FROM Uztrous u WHERE UPPER(u.uztrousEstado) LIKE UPPER(:uztrousEstado)");
+            query.setParameter("uztrousEstado", "%" + uztrousEstado + "%");
+            return query.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //MÉTODO PARA  rous de acuerdo a id rol y pidm usuario
+    public List<Uztrous> findRousIdRolPidmUsuario(BigDecimal uztrolId, BigDecimal uztuserPidm) {
+        try {
+            Query query = em.createQuery("SELECT u FROM Uztrous u WHERE u.uztrousPK.uztrolId = :uztrolId and  u.uztrousPK.uztuserPidm = :uztuserPidm");
+            query.setParameter("uztrolId", uztrolId);
+            query.setParameter("uztuserPidm", uztuserPidm);
+            return query.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //MÉTODO PARA  rous de acuerdo a id rol y pidm usuario pero con nombres
+    //MÉTODO PARA  rous de acuerdo a id rol y pidm usuario pero con nombres
+    public List<Object[]> findRousIdRolPidmUsuarioNombre(BigDecimal uztrolId, BigDecimal uztuserPidm) {
+        try {
+            Query query = em.createQuery("SELECT r.uztrolNombre,u.uztuserNombres,ru.uztrousFcha,ru.uztrousEstado "
+                    + "FROM Uztrol r, Uztuser u, Uztrous ru where ru.uztrousPK.uztrolId=r.uztrolId and ru.uztrousPK.uztuserPidm=u.uztuserPidm "
+                    + "and ru.uztrousPK.uztrolId = :uztrolId and ru.uztrousPK.uztuserPidm = :uztuserPidm");
+            query.setParameter("uztrolId", uztrolId);
+            query.setParameter("uztuserPidm", uztuserPidm);
+            return query.getResultList();
+
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+;
 }
